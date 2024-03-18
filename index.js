dc277a67070cbb83204ed7b3da7305b5d3df0686
Joseph-Chege <mjchege23@gmail.com>
2024-03-18 15:49:08 +0300

    Created the +,-,* and / functions as wellas the parseInt and parseFloat functions

---
+++ index.js
@@ -0,0 +1,51 @@
+function add(a, b){
+    return (a + b);
+}
+function subtract(a, b) {
+    return (a - b);
+}
+function multiply(a, b) {
+    return (a * b);
+}
+function divide(a, b) {
+    return (a / b);
+}
+
+let number = 5;
+
+function increment(number){
+    return (number += 1);
+}
+function decrement(number){
+    return (number -= 1);
+}
+
+let n =  parseInt(`3`, 10); 
+
+function makeInt(n) {
+    return (n);
+   
+}
+
+let num = parseInt(`nonsense!`, 10);
+
+function makeInt(num) {
+    return (num);
+    
+}
+
+let numb = "2.222";
+
+function preserveDecimal(numb) {
+    numb = parseFloat(numb);
+    return (numb);
+
+}
+
+let numbe =  "0x2328";
+
+function makeInt(numbe) {
+    numbe = parseInt(numbe, 10);
+    return (numbe);
+   
+}
